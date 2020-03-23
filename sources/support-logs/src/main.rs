f5d66fa73802f0d0570d6ac86a8f328e055ddf51
Matt Briggs <matthew.james.briggs@gmail.com>
2020-03-23 13:54:33 -0700

    cleaner

--- sources/support-logs/src/main.rs
+++ sources/support-logs/src/main.rs
@@ -3,45 +3,20 @@
 mod error;
 
 use crate::error::Result;
-// use crate::error::Result;
 
 use std::process::{Command, Stdio};
 use std::fs::File;
 use snafu::ResultExt;
-// use crate::error::FileError;
 use std::path::PathBuf;
 use std::str::FromStr;
-use std::fs;
-
-// const TEST_SCRIPT: &str = r####"#!/bin/bash
-// echo "1 stdout"
-// >&2 echo "2 stderr"
-// echo "3 stdout"
-// >&2 echo "4 stderr"
-// echo "5 stdout"
-// "####;
 
 fn main() -> Result<()> {
-    // let outputs = File::create("out.txt").context(crate::error::FileError { path: "out.txt".to_string() })?;
-    // let errors = outputs.try_clone().context(crate::error::FileError { path: "out.txt".to_string() })?;
-
-    let pbuf = PathBuf::from_str("./test-script.sh").unwrap();
-    let pbuf = fs::canonicalize(&pbuf).unwrap();
-
-    // Command::new("/bin/bash")
-    //     .args(&["-c", pbuf.to_str().unwrap()])
-    //     .stdout(Stdio::from(outputs))
-    //     .stderr(Stdio::from(errors))
-    //     .spawn().context(crate::error::FileError { path: "out.txt".to_string() })?
-    //     .wait_with_output().context(crate::error::FileError { path: "out.txt".to_string() })?;
-
     run(CommandInfo {
-        command: "/bin/bash",
-        args: vec!("-c", pbuf.to_str().unwrap()),
+        command: "echo",
+        args: vec!("arg1", "arg2"),
         output_filename: "x.log",
-        output_dir: PathBuf::from_str(".").unwrap(),
+        output_dir: PathBuf::from_str("/tmp").unwrap(),
     })
-    // Ok(())
 }
 
 struct CommandInfo<'a> {
@@ -51,11 +26,8 @@ struct CommandInfo<'a> {
     output_dir: PathBuf,
 }
 
-// const PATH_MESSAGE: &str = "Unable to build filepath for '{}'";
-
 fn run<'a>(command_info: CommandInfo<'a>) -> Result<()> {
     let opath = command_info.output_dir.join(command_info.output_filename);
-    // let opath = opath.canonicalize().context(error::FileError { path: opath.to_str().unwrap() })?;
     let opath_str = opath.to_str().ok_or(error::Error::ErrorMessage { message: format!("Unable to build filepath for '{}'", command_info.output_filename).to_string() })?;
     let ofile = File::create(opath_str).context(crate::error::FileError { path: opath_str.to_string() })?;
     let efile = ofile.try_clone().context(crate::error::FileError { path: opath_str.to_string() })?;
