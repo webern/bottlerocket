e204023cbf910df194bbaf7ca654efb1cffa1d42
Matt Briggs <matthew.james.briggs@gmail.com>
2020-03-23 15:30:34 -0700

    with program arguments

--- sources/support-logs/src/main.rs
+++ sources/support-logs/src/main.rs
@@ -7,15 +7,48 @@ mod error;
 mod exec_to_file;
 
 use crate::error::Result;
+use snafu::ErrorCompat;
 use std::path::PathBuf;
-use std::str::FromStr;
+use structopt::StructOpt;
 
+#[derive(Debug, StructOpt)]
+pub struct ProgramArgs {
+    /// The compressed output file that will be written containing all of the support logs.
+    #[structopt(short = "o", long = "output", default_value = "/tmp/support-logs.tar.gz")]
+    output: PathBuf,
 
-fn main() -> Result<()> {
-    crate::exec_to_file::exec_to_file(crate::exec_to_file::ExecToFile {
+    /// The temporary working directory where log files will be written before being aggregated.
+    #[structopt(short = "t", long = "tempdir", default_value = "/tmp/support-logs")]
+    tempdir: PathBuf,
+}
+
+// TODO: https://rust-lang-nursery.github.io/rust-cookbook/compression/tar.html
+
+fn main() -> ! {
+    let args = ProgramArgs::from_args();
+    std::process::exit(match run_program(&args) {
+        Ok(()) => 0,
+        Err(err) => {
+            eprintln!("{}", err);
+            if let Some(var) = std::env::var_os("RUST_BACKTRACE") {
+                if var != "0" {
+                    if let Some(backtrace) = err.backtrace() {
+                        eprintln!("\n{:?}", backtrace);
+                    }
+                }
+            }
+            1
+        }
+    })
+}
+
+fn run_program(args: &ProgramArgs) -> Result<()> { crate::exec_to_file::exec_to_file(make_fake_command(args)) }
+
+fn make_fake_command(args: &ProgramArgs) -> crate::exec_to_file::ExecToFile<'static> {
+    crate::exec_to_file::ExecToFile {
         command: "echo",
         args: vec!("arg1", "arg2"),
-        output_filename: "x.log",
-        output_dir: PathBuf::from_str("/tmp").unwrap(),
-    })
+        output_filename: "fake-stuff.log",
+        output_dir: args.tempdir.clone(),
+    }
 }
