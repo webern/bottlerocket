a50fd59b9dc5574158d510da4f1dcd8a36cecd48
Matt Briggs <matthew.james.briggs@gmail.com>
2020-03-25 15:44:58 -0700

    remove structopt

--- sources/logdog/src/main.rs
+++ sources/logdog/src/main.rs
@@ -10,21 +10,67 @@ mod create_tarball;
 use crate::error::Result;
 use snafu::ErrorCompat;
 use std::path::PathBuf;
-use structopt::StructOpt;
+use std::{env, process};
 
-#[derive(Debug, StructOpt)]
 pub struct ProgramArgs {
-    /// The compressed output file that will be written containing all of the support logs.
-    #[structopt(short = "o", long = "output", default_value = "/tmp/logdog.tar.gz")]
     output: PathBuf,
-
-    /// The temporary working directory where log files will be written before being aggregated.
-    #[structopt(short = "t", long = "tempdir", default_value = "/tmp/logdog")]
     tempdir: PathBuf,
 }
 
+/// Print a usage message in the event a bad arg is passed
+fn usage() -> ! {
+    let program_name = env::args().next().unwrap_or_else(|| "program".to_string());
+    eprintln!(
+        r"Usage: {}
+            [ --tempdir PATH directory to write logs to temporarily before zipping ]
+            [ --output PATH file to write zipped logs to ]
+",
+        program_name,
+    );
+    process::exit(2);
+}
+
+/// Prints a more specific message before exiting through usage().
+fn usage_msg(msg: &str) -> ! {
+    eprintln!("{}\n", msg);
+    usage();
+}
+
+/// Parses the command line arguments and provides defaults for those that are optional.
+fn parse_args(args: env::Args) -> ProgramArgs {
+    let mut tempdir_arg_str = None;
+    let mut output_arg_str = None;
+
+    let mut iter = args.skip(1);
+    while let Some(arg) = iter.next() {
+        match arg.as_ref() {
+            "--tempdir" => {
+                tempdir_arg_str = Some(
+                    iter.next()
+                        .unwrap_or_else(|| usage_msg("Did not give argument to --tempdir")),
+                )
+            }
+
+            "--output" => {
+                output_arg_str = Some(
+                    iter.next()
+                        .unwrap_or_else(|| usage_msg("Did not give argument to --output")),
+                )
+            }
+
+            _ => usage(),
+        }
+    }
+
+    // TODO - handle None for these with default values
+    ProgramArgs {
+        tempdir: PathBuf::from(tempdir_arg_str.unwrap()),
+        output: PathBuf::from(output_arg_str.unwrap()),
+    }
+}
+
 fn main() -> ! {
-    let args = ProgramArgs::from_args();
+    let args = parse_args(env::args());
     std::process::exit(match run_program(&args) {
         Ok(()) => 0,
         Err(err) => {
