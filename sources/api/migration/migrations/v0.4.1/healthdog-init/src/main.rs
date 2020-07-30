b2a03c98a3ded950dca6afeb7f5cb22f4a7f3e36
Matt Briggs <brigmatt@amazon.com>
2020-07-30 10:30:54 -0700

    add migration

---
+++ sources/api/migration/migrations/v0.4.1/healthdog-init/src/main.rs
@@ -0,0 +1,24 @@
+#![deny(rust_2018_idioms)]
+
+use migration_helpers::common_migrations::AddSettingsMigration;
+use migration_helpers::{migrate, Result};
+use std::process;
+
+/// We added two new settings, `updates.version-lock` and `updates.ignore-waves`
+fn run() -> Result<()> {
+    migrate(AddSettingsMigration(&[
+        "settings.healthdog.metrics-url",
+        "settings.healthdog.send-metrics",
+        "settings.healthdog.service-health",
+    ]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
