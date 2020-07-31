69716eae132dad5b1d560ff7a87081506ed33cd6
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-07-30 17:05:11 -0700

    Update sources/healthdog/src/main.rs

--- sources/healthdog/src/main.rs
+++ sources/healthdog/src/main.rs
@@ -6,7 +6,41 @@ It does so by sending key-value pairs as query params in an HTTP GET request.
 
 # What it Sends
 
-TODO - list of the metrics being sent.
+```suggestion
+The standard set of metrics:
+* `sender`: the application sending the report.
+* `event`: the event that invoked the report.
+* `version`: the Bottlerocket version.
+* `variant`: the Bottlerocket variant.
+* `arch`: the machine architecture, e.g.'x86_64' or 'arm'.
+* `region`: the region the machine is running in.
+* `seed`: the seed value used to roll-out updates.
+* `version-lock`: the optional setting that locks Bottlerocket to a certain version.
+* `ignore-waves`: an update setting that allows hosts to update before their seed is reached.
+
+Additionally, when `healthdog` sends a 'health ping', it adds:
+* `is-healthy`: true or false based on whether critical services are running.
+* `failed_services`: a list of critical services that have failed, if any.
+
+# Configuration
+
+The following configuration options are available, and read by `healthdog` from a `toml` file that looks like this:
+
+```toml
+# the url to which healthdog will send metrics information
+metrics_url = "https://example.com/metrics"
+# whether or not healthdog will send metrics. opt-out by setting this to false
+send_metrics = true
+# a list of systemd service names that will be checked
+service_health = ["apiserver", "containerd", "kubelet"]
+# the region
+region = "us-west-2"
+# the update wave seed
+seed = 1234
+# what version bottlerocket should stay on
+version_lock = "latest"
+# whether bottlerocket should ignore update roll-out timing
+ignore_waves = false
 
 */
 
